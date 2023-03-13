be275d257dbd5a718aa94cddff0cf50a94b4e3c6
IsakSkeie <isak.skeie@goodtech.no>
2023-03-13 22:35:16 +0100

    scaled data

--- 4.MultivariteDataAnalysis/ModelValidation.py
+++ 4.MultivariteDataAnalysis/ModelValidation.py
@@ -7,15 +7,17 @@ import pandas as pd
 
 path  =  "ValidatingData.xlsx"
 df = pd.read_excel(path)
-
-Atributes = ['RIS-QI01_T', 'IPU-FB18', 'SAN3-FB01', 'ANL1-QI02', 
+#df= (df-df.mean())/df.std()
+df = df.apply(lambda column: (column -column.mean())/column.std())
+#normalized_df=(df-df.min())/(df.max()-df.min())
+Atributes = ['RIS-QI01_TT', 'IPU-FB18', 'SAN3-FB01', 'ANL1-QI02', 
              'ANL1-QI01', 'SAN3-ML01', 'POL_FT04', 'IPU_LT01', 
              'SED5-QI11']
 Y_Name = 'SED5-QI01'
 n = len(Atributes)
 #%% [markdown]
 #Plot validating Turbidity
-column = 'POL_FT04.POL_FT04 VERDI:Value'
+column = 'POL_FT04'
 result = df[column]
 fig = plt.figure(figsize=(10, 6))
 plt.grid()
@@ -27,24 +29,17 @@ plt.plot(result, linewidth=0.35)
 
 # %% [markdown]
 # Creating the polynomial
-df_coeff = pd.read_excel("ModelCoefficients.xlsx")
-Coefficients = np.zeros(n)
 
-for i in range(n):
-    Coefficients[i] = df_coeff[Atributes[i]]    
-Coefficients = Coefficients.T
-#%%
-def RunModel(Coefficients, ValData, Atributes):
-    Y = np.zeros(len(df.index))
-    k = 0
-    for instance in ValData:
-        Y_k = 0
-        for i in range(n):
-            Y_k = Y_k + instance[Atributes[i]]*Coefficients[Atributes[i]]
-        Y[k] = Y_k
-    return Y
+df_coeff = pd.read_excel("ModelCoefficients.xlsx")
+
+m = len(df_coeff.index)
+
+Y = np.zeros([m, len(df.index)])
+for atrb in range(m):
+    for i in range(n):
+        Y[atrb] =  Y[atrb] + df[Atributes[i]]*df_coeff[Atributes[i]].loc[atrb]
+
 # %%
 
-Y = RunModel(Coefficients, df, Atributes)
-
+plt.plot(Y[:,0])
 # %%
