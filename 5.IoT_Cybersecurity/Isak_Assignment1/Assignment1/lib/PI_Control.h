8aa76d2e0ba43945df5e490ea3eb68a57f42e264
IsakSkeie <isak.skeie@goodtech.no>
2023-08-18 14:58:53 +0200

    Cleaned up project folder, programmed p and PI con

---
+++ 5.IoT_Cybersecurity/Isak_Assignment1/Assignment1/lib/PI_Control.h
@@ -0,0 +1,33 @@
+#ifndef CustomControlLib_h
+#define CustomControlLib_h
+
+#include <Arduino.h>
+
+
+class Control {
+public:
+    //Declared variables that could be useful outside class
+    float error;
+    float elapsedTime;
+    float CV;
+
+
+    Control(float SP, float Kp, float Ki);
+    float Control_P(float PV);
+    float Control_PI(float PV);
+
+private:
+//Declares var for inputs
+
+    float _SP;
+    float _Kp;
+    float _Ki;
+//Declares variables for scantime based derivative    
+    unsigned long _lastTime;
+    unsigned long _currentTime;
+
+    float _iSum;
+    
+}
+
+#endif
