e8f56f381fe2e823fb0ca9652f59a5ab36f621df
IsakSkeie <isak.skeie@goodtech.no>
2023-08-23 20:11:14 +0200

    MPC: Low pass filter

--- 5.IoT_Cybersecurity/Isak_Assignment1/Assignment1/lib/Control/PI_Control.cpp
+++ 5.IoT_Cybersecurity/Isak_Assignment1/Assignment1/lib/Control/PI_Control.cpp
@@ -9,8 +9,8 @@ Control::Control(float SP, float Kp, float Ki)
     _SP         = SP;
     _Kp         = Kp;
     _Ki         = Ki;
-    _lastTime   = millis();
-    _iSum       = 0;
+    _PI_lastTime   = millis();
+    _PI_iSum       = 0;
 }
 
 float Control::Control_P(float PV) 
@@ -25,13 +25,27 @@ float Control::Control_P(float PV)
 float Control::Control_PI(float PV)
 {
     unsigned long currentTime   = millis();
-    float elapsedTime = (currentTime - _lastTime)/1000.0;
+    float elapsedTime = (currentTime - _PI_lastTime)/1000.0;
     error = _SP - PV;
 
-    _iSum = _iSum + error;
+    _PI_iSum = _PI_iSum + error;
 
-    CV = _Kp*error + _Ki*_iSum;
+    CV = _Kp*error + _Ki*_PI_iSum;
     //CV = constrain(CV, 0, 10000);
-    _lastTime = currentTime;
+    _PI_lastTime = currentTime;
     return CV;
+}
+
+float Control::LPF(float PV)
+{
+    unsigned long currentTime   = millis();
+    float dt = (currentTime - _LPF_lastTime)/1000.0;
+    float rc = 1.0/(2.0 * M_PI * CutOff);
+    alpha   = dt / (dt + rc);
+
+    _LPF_Output     = (1-alpha)*_LPF_PrevOutput + alpha*PV; 
+    _LPF_lastTime   = currentTime;
+    _LPF_PrevOutput = _LPF_Output;
+
+    return _LPF_Output
 }
