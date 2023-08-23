e3126f88688916e4527c0c859f4b69505c0ae010
IsakSkeie <isak.skeie@goodtech.no>
2023-08-23 20:11:36 +0200

    MPC: LPF

--- 5.IoT_Cybersecurity/Isak_Assignment1/Assignment1/lib/Control/PI_Control.h
+++ 5.IoT_Cybersecurity/Isak_Assignment1/Assignment1/lib/Control/PI_Control.h
@@ -8,10 +8,14 @@ public:
     float error;
     float elapsedTime;
     float CV;
+    float alpha; //Filter coefficient
+    float CutOff = 1000; //Cut off frequency for LPF
+    
 
     Control(float SP, float Kp, float Ki);
     float Control_P(float PV);
     float Control_PI(float PV);
+    float LPF(float PV);
 
 private:
 //Declares var for inputs
@@ -20,9 +24,12 @@ private:
     float _Kp;
     float _Ki;
 //Declares variables for scantime based derivative    
-    unsigned long _lastTime;
-    unsigned long _currentTime;
-
-    float _iSum;
+    unsigned long _PI_lastTime;
+    unsigned long _PI_currentTime;
+    float _PI_iSum;
     
+    float _LPF_PrevOutput;
+    float _LPF_Output;
+    unsigned long _LPF_lastTime;
+    unsigned long _LPF_currentTime;
 };
