5028f3e9cf47834aa6f4db9a721152941dba315f
IsakSkeie <isak.skeie@goodtech.no>
2023-08-24 07:57:08 +0200

    Created project for air heater assignment

---
+++ 5.IoT_Cybersecurity/Isak_Assignment1/Assignment1_ HeatSim/src/main.cpp
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+
+// put function declarations here:
+float HeaterSim(float);
+
+void setup() {
+  // put your setup code here, to run once:
+  float result = HeaterSim(3);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+// put function definitions here:
+float HeaterSim(float sp) {
+
+float placeholder;
+  return placeholder;
+}
