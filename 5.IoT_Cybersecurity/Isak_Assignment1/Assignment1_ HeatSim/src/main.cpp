92ae170d0a8c843b5d379d0a551c2eb46642ec51
IsakSkeie <isak.skeie@goodtech.no>
2023-08-24 10:05:44 +0200

    IoT: Moved project files

--- 5.IoT_Cybersecurity/Isak_Assignment1/Assignment1_ HeatSim/src/main.cpp
+++ 5.IoT_Cybersecurity/Isak_Assignment1/Assignment1_ HeatSim/src/main.cpp
@@ -1,22 +1,27 @@
 #include <Arduino.h>
 
-// put function declarations here:
-float HeaterSim(float);
+// // put function declarations here:
+ float HeaterSim(float);
 
-void setup() {
-  // put your setup code here, to run once:
-  float result = HeaterSim(3);
+ void setup() {
+   // put your setup code here, to run once:
+   float result = HeaterSim(3);
   
-}
+ }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+ void loop() {
+   // put your main code here, to run repeatedly:
+   delay(2000);
+   Serial.println("Debug");
 
-}
+ }
 
-// put function definitions here:
-float HeaterSim(float sp) {
+ // put function definitions here:
+ float HeaterSim(float sp) {
 
-float placeholder;
-  return placeholder;
-}
+ float placeholder;
+   return placeholder;
+ }
+
+
+
