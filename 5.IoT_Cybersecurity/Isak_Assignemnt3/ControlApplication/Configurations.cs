80135a6ce9c23da4746a32590c35517a42c29602
IsakSkeie <isak.skeie@goodtech.no>
2023-11-09 20:10:25 +0100

    Ready python files for control

---
+++ 5.IoT_Cybersecurity/Isak_Assignemnt3/ControlApplication/Configurations.cs
@@ -0,0 +1,35 @@
+﻿public class GlobalSettings
+{
+    private static GlobalSettings _instance;
+    private static object _lock = new object();
+
+    // Private constructor to prevent direct instantiation
+    private GlobalSettings()
+    {
+        // Initialize your variables here
+        SimulationName = "Default Simulation";
+        SimulationDuration = 10;
+    }
+
+    public static GlobalSettings Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new GlobalSettings();
+                    }
+                }
+            }
+            return _instance;
+        }
+    }
+
+    // Define your globally accessible variables as public properties
+    public string SimulationName { get; set; }
+    public int SimulationDuration { get; set; }
+}
