7ceafe27dd6430ddfb3269e3f38a7904e9c2daf0
IsakSkeie <isak.skeie@goodtech.no>
2023-11-11 19:47:29 +0100

    Working Data retrieval for website

---
+++ 5.IoT_Cybersecurity/Isak_Assignemnt3/NewDataMonitor/AppDBContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataMonitor
+{
+    public class AppDBContext : DbContext
+    {
+
+        public IConfiguration _config { get; set; }
+
+        public AppDBContext(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_config.GetConnectionString("DatabaseConnection"));
+        }
+
+        public DbSet<TemperatureData> Temperatures { get; set; } 
+    }
+}
---
+++ 5.IoT_Cybersecurity/Isak_Assignemnt3/NewDataMonitor/TemperatureData.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataMonitor
+{
+    public class TemperatureData
+    {
+
+        [Key] // Specify the primary key
+        public int Id { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public float PV { get; set; }
+        public float SP { get; set; }
+    }
+}
